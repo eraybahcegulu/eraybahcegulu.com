c5a4bf8e01eb689177cc1a664a6a85d57e91bb6c
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-09-21 20:06:42 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,21 @@
 import Main from "./components/Main.jsx";
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-5N3FNKYL58");
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
 
-  ReactGA.initialize("G-5N3FNKYL58");
-
-
-  ReactGA.send({ hitType: "pageview", page: "/my-path" });
-  
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Main} />
       </Switch>
     </Router>
-
   );
 }
 
